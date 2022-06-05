c04029f111866052bc990f04cc388fa13466ab27
Akzya <106802261+Akzya@users.noreply.github.com>
2022-06-05 22:51:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -817,9 +817,10 @@ let cheatMenuCode = `
 		<center>Assistant v0.1</center><hr>
 
 		<div id="gameStates" style="display: none;">
-            <p>Striker Hack: <font id="strikerHackStateColor" color="red"><label id="strikerHackState">OFF</label></font></p>
+            <p>Striker AimBot: <font id="sdffdsffd" color="green"><label id="scsdfcdst">ON</label></font></p>
             <p>Anti-Aim: <font id="antiAimStateColor" color="red"><label id="antiAimState">OFF</label></font></p>
-            <p>Striker Hacks: <font id="dvdv" color="green"><label id="nahaha">ON</label></font></p>
+	    <p>Striker One Shot Toggle:  <font id="strikerHackStateColor" color="green"><label id="strikerHackState">End</label></font></p>
+            <p><font id="strikerHackStateColor" color="green"><label id="strikerHackState">AimBot Is Always On</label></font></p>
             <p>Remove Mines: <font id="removeMinesStateColor" color="green"><label id="removeMinesState">ON</label></font></p>
 		</div>
 
